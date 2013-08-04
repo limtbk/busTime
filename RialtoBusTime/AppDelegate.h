ae04dca3ebca5de3a3991a3dab7969b4651621af
limtbk <lim.tbk@gmail.com>
2013-08-04 17:56:38 +0300

    Initial Commit

---
+++ RialtoBusTime/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RialtoBusTime
+//
+//  Created by lim on 8/4/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RialtoBusTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RialtoBusTime
+//
+//  Created by lim on 8/4/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
